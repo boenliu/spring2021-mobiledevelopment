fafe109c3507709dc26d7efb799b2025464e3b6c
boenliu <67018542+boenliu@users.noreply.github.com>
2021-03-19 13:56:57 -0400

    Week10
    Created a new react native app with two components: name and food.

---
+++ week10/components/food/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Food extends React.Component{
+  render(){
+    return(
+      <View>
+        <Text>a fat juicy burger</Text>
+      </View>
+    );
+  }
+}
---
+++ week10/components/name/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Name extends React.Component{
+  render(){
+    return(
+      <View>
+        <Text>BoEn Liu</Text>
+      </View>
+    );
+  }
+}
